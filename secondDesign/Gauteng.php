1b46fec61c6ac5643cb829896e4cf91f305fa1fc
bdube83 <bdube83@gmail.com>
2016-06-19 00:16:04 +0200

    remove old content.

--- secondDesign/Gauteng.php
+++
@@ -1,4 +0,0 @@
-<div style = "text-align: center;">
-    <span style="display: inline-block; height: 100%; vertical-align: middle;"></span>
-    <img src="Gauteng.png" style="vertical-align: middle;"/>
-</div>
--- secondDesign/North_West.php
+++
@@ -1,4 +0,0 @@
-<div style = "text-align: center;">
-    <span style="display: inline-block; height: 100%; vertical-align: middle;"></span>
-    <img src="North West.png" style="vertical-align: middle;"/>
-</div>
